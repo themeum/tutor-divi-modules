8653cb091e29674853d50eb13847158e66ec00d4
Shewa <shewa@Shewas-iMac.local>
2021-02-26 18:11:58 +0600

    course price module file added

---
+++ includes/modules/CoursePrice/CoursePrice.jsx
@@ -0,0 +1,15 @@
+import React ,{ Component, Fragment } from "react";
+
+class CoursePrice extends Component {
+
+    static slug = "tutor_course_price";
+
+    render() {
+        return (
+            <Fragment>
+                <h3>Course Price</h3>
+            </Fragment>            
+        );
+    }
+}
+export default CoursePrice;
