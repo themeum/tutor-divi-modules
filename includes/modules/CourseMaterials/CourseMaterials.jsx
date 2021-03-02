a703ad003c81d03762c0d00c9dcb6fceef5d9081
Shewa <shewa@Shewas-iMac.local>
2021-03-02 13:51:32 +0600

    course material template, settings, jsx added

--- includes/modules/CourseMaterials/CourseMaterials.jsx
+++ includes/modules/CourseMaterials/CourseMaterials.jsx
@@ -6,11 +6,18 @@ class CourseMaterials extends Component {
     static slug = 'tutor_course_materials';
 
     render() {
+        const utils = window.ET_Builder.API.Utils;
+        const icon = utils.processFontIcon(this.props.icon);
         return (
             <Fragment>
-                <div
-                    dangerouslySetInnerHTML={{ __html: this.props.__materials }} 
-                />
+            <div className="tutor-single-course-segment  tutor-course-material-includes-wrap">
+                <h4 className="tutor-segment-title"> Materials Includes </h4>
+                <div className="tutor-course-target-audience-content">
+                    <ul className="tutor-course-target-audience-items tutor-custom-list-style">
+
+                    </ul>
+                </div>
+            </div>
             </Fragment>
         );
     }
--- includes/modules/CourseMaterials/CourseMaterials.php
+++ includes/modules/CourseMaterials/CourseMaterials.php
@@ -39,13 +39,16 @@ class TutorCourseMaterials extends ET_Builder_Module {
 			'advanced' => array(
 				'toggles' => array(
 					'title' => array(
-						'title'    => esc_html__('Title', 'tutor-divi-modules'),
+						'title'		=> esc_html__('Section Title', 'tutor-divi-modules'),
+					),
+					'list'	=> array(
+						'title'		=> esc_html__( 'List', 'tutor-divi-modules' ),
 					),
 					'icon' => array(
-						'title'    => esc_html__('Icon', 'tutor-divi-modules'),
+						'title'		=> esc_html__('Icon', 'tutor-divi-modules'),
 					),
 					'text' => array(
-						'title'    => esc_html__('Text', 'tutor-divi-modules'),
+						'title'		=> esc_html__('Text', 'tutor-divi-modules'),
 					),
 				),
 			),
@@ -65,13 +68,6 @@ class TutorCourseMaterials extends ET_Builder_Module {
 					'tab_slug'     => 'advanced',
 					'toggle_slug'  => 'title',
 				),
-				'icon' => array(
-					'css'          => array(
-						'main' => $icon_selector,
-					),
-					'tab_slug'     => 'advanced',
-					'toggle_slug'  => 'icon',
-				),
 				'text' => array(
 					'css'          => array(
 						'main' => $text_selector,
@@ -80,6 +76,14 @@ class TutorCourseMaterials extends ET_Builder_Module {
 					'toggle_slug'  => 'text',
 				),
 			),
+			'borders'	=> array(
+				'list'			=> array(
+					'css'			=> '',
+					'tab_slug'		=> 'advanced',
+					'toggle_slug'	=> 'list'
+				)
+			),
+
 		);
 	}
 
@@ -104,7 +108,7 @@ class TutorCourseMaterials extends ET_Builder_Module {
 				'type'                => 'computed',
 				'computed_callback'   => array(
 					'TutorCourseMaterials',
-					'get_content',
+					'get_props',
 				),
 				'computed_depends_on' => array(
 					'course'
@@ -113,9 +117,120 @@ class TutorCourseMaterials extends ET_Builder_Module {
 					'course',
 				),
 			),
+			//general settings content tab
+			'label'		=> array(
+				'label'				=> esc_html__( 'Material Label', 'tutor-divi-modules'),
+				'type'				=> 'text',
+				'default'			=> esc_html__( 'Material Includes', 'tutor-divi-modules' ),
+				'option_category'	=> 'basic',
+				'toggle_slug'		=> 'main_content'
+	
+			),
+			'layout'	=> array(
+				'label'				=> esc_html( 'Layout', 'tutor-divi-modules' ),
+				'type'				=> 'select',
+				'options'			=> array(
+					'list'		=> esc_html__( 'List', 'tutor-divi-modules' ), 
+					'inline'	=> esc_html__( 'Inline', 'tutor-divi-modules' ), 
+				),
+				'default'			=> 'list',
+				'option_category'	=> 'layout',
+				'toggle_slug'		=> 'main_content',
+				'mobile_options'	=> true
+			),
+			'icon' => array(
+				'label'             => esc_html__( 'Icon', 'tutor-divi-modules' ),
+				'type'              => 'select_icon',
+				'default'			=> 'N',
+				'class'				=> array( 'et-pb-font-icon' ),
+				'option_category'   => 'basic_option',
+				'toggle_slug'     	=> 'main_content',		
+			),
+			'alignment'		=> array(
+				'label'				=> esc_html__('Alignment', 'tutor-divi-modules'),
+				'type'				=> 'text_align',
+				'option_category'	=> 'configuration',
+				'options'			=> et_builder_get_text_orientation_options( array( 'justified' ) ),
+				'default'			=> 'left',
+				'toggle_slug'		=> 'main_content',
+				'mobile_options'	=> true
+			),
+			//advanced tab section title toggles
+			'gap'			=> array(
+				'label'				=> esc_html__( 'Gap', 'tutor-divi-modules' ),
+				'type'				=> 'range',
+				'default'			=> '10',
+				'default_unit'		=> 'px',
+				'range_settings'	=> array(
+					'min'	=> 1,
+					'max'	=> 100,
+					'step'	=> 1
+				),
+				'tab_slug'			=> 'advanced',
+				'toggle_slug'		=> 'title',
+				'mobile_options'	=> true
+			),
+			//advanced tab section list toggles
+			'space_between'	=> array(
+				'label'				=> esc_html__( 'Space Between', 'tutor-divi-modules' ),
+				'type'				=> 'range',
+				'default'			=> '10',
+				'default_unit'		=> 'px',
+				'range_settings'	=> array(
+					'min'	=> 1,
+					'max'	=> 100,
+					'step'	=> 1
+				),
+				'tab_slug'			=> 'advanced',
+				'toggle_slug'		=> 'list',
+				'mobile_options'	=> true				
+			),
+			'padding'		=> array(
+				'label'           	=> esc_html__( 'Padding', 'tutor-divi-modules' ),
+				'type'            	=> 'custom_padding',
+				'hover'           	=> 'tabs',
+				'option_category' 	=> 'layout',
+				'tab_slug'        	=> 'advanced',
+				'toggle_slug'     	=> 'list',
+				'allowed_units'   	=> array( '%', 'em', 'rem', 'px', 'cm', 'mm', 'in', 'pt', 'pc', 'ex', 'vh', 'vw' ),
+			),
+			//advanced tab text toggle
+			'color'			=> array(
+				'label'				=> esc_html__( 'Color', 'tutor-divi-modules' ),
+				'type'				=> 'color-alpha',
+				'tab_slug'			=> 'advanced',
+				'toggle_slug'		=> 'icon'
+			),
+			'size'			=> array(
+				'label'				=> esc_html__( 'Size', 'tutor-divi-modules' ),
+				'type'				=> 'range',
+				'default'			=> '12',
+				'default_unit'		=> 'px',
+				'range_settings'	=> array(
+					'min'	=> '1',
+					'max'	=> '100',
+					'step'	=> '1'
+				),
+				'tab_slug'			=> 'advanced',
+				'toggle_slug'		=> 'icon',
+				'mobile_option'		=> true
+			),
+
 		);
 
 		return $fields;
+	}
+	
+	/**
+	 * get material props
+	 */
+	public static function get_props( $args = [] ) {
+		$course_id	= $args['course'];
+		$materials	= tutor_course_material_includes( $course_id );
+		$props		= array(
+			'materials'	=> $materials
+		);
+		return $materials;
 	}
 
 	/**
@@ -125,12 +240,13 @@ class TutorCourseMaterials extends ET_Builder_Module {
 	 */
 	public static function get_content($args = []) {
 		$course = Helper::get_course($args);
-		$output = '';
-		if ($course) {
-			$output = tutor_course_material_includes_html(false); //echo false
-		}
 
-		return $output;
+		ob_start();
+		if( $course ) {
+			include_once ( dtlms_get_template('course/material') );
+		}
+		
+		return ob_get_clean();
 	}
 
 	/**
@@ -145,6 +261,22 @@ class TutorCourseMaterials extends ET_Builder_Module {
 	 * @return string module's rendered output
 	 */
 	public function render($attrs, $content = null, $render_slug) {
+		//selectors
+		$wrapper			= '%%order_class%% .tutor-course-material-includes-wrap';
+		$li_selector		= $wrapper." li";
+
+		//props
+
+		//set styles
+		ET_Builder_Element::set_style(
+			$render_slug,
+			array(
+				'selector'		=> $li_selector."::before",
+				'declaration'	=> 'content: none;'
+			)
+		);
+
+		//set styles end
 
 		$output = self::get_content($this->props);
 
@@ -154,6 +286,7 @@ class TutorCourseMaterials extends ET_Builder_Module {
 		}
 
 		return $this->_render_module_wrapper($output, $render_slug);
+
 	}
 }
 
