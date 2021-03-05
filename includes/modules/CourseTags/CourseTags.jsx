eaf18dac2fb29643c827705b29ac289efd53c42f
Shewa <shewa@Shewas-iMac.local>
2021-03-05 18:19:19 +0600

    course tags template added

--- includes/modules/CourseTags/CourseTags.jsx
+++ includes/modules/CourseTags/CourseTags.jsx
@@ -4,10 +4,30 @@ class CourseTags extends Component {
 
     static slug = "tutor_course_tags";
 
+    tagTemplate(tags) {
+        if(tags.length) {
+            const tag = tags.map((tag) => {
+                return <a href={tag.slug}>{ tag.name }</a>
+            });
+            return tag;
+        }
+    }
+
     render() {
+        if(!this.props.__tags) {
+            return '';
+        }
+     
         return (
             <Fragment>
-                <h3>Course Tags</h3>
+                <div class="tutor-single-course-segment">
+                    <div class="course-benefits-title">
+                        <h4 class="tutor-segment-title"> { this.props.label } </h4>
+                    </div>
+                    <div class="tutor-course-tags">
+                        { this.tagTemplate(this.props.__tags.tags) }
+                    </div>
+                </div>
             </Fragment>            
         );
     }
--- includes/modules/CourseTags/CourseTags.php
+++ includes/modules/CourseTags/CourseTags.php
@@ -79,17 +79,72 @@ class CourseTags extends ET_Builder_Module {
 
     public function get_fields() {
 		return array(
+			'course'       	=> Helper::get_field(
+				array(
+					'default'          => Helper::get_course_default(),
+					'computed_affects' => array(
+						'__share',
+					),
+				)
+			),
+			'__tags'		=> array(
+				'type'                => 'computed',
+				'computed_callback'   => array(
+					'CourseTags',
+					'get_props',
+				),
+				'computed_depends_on' => array(
+					'course'
+				),
+				'computed_minimum'    => array(
+					'course',
+				),
+			),
+			//general settings tab main_content toggle			
 			'label'     => array(
 				'label'           	=> esc_html__( 'Label', 'tutor-divi-modules' ),
 				'type'            	=> 'text',
+				'default'			=> esc_html__( 'Course Tags', 'tutor-divi-modules'),
 				'option_category' 	=> 'basic_option',
 				'toggle_slug'     	=> 'main_content',
 			),
 		);
     }
 
+	/**
+	 * computed value
+	 * @return string | array course level
+	 */
+	public static function get_props( $args = [] ) {
+		$course_id 	= $args[ 'course' ];
+		$tags 		= get_tutor_course_tags( $course_id );
+		$props 		= array(
+			'tags'	=> $tags
+		);
+		return $props;
+	}
+
+	/**
+	 * Get the tutor course author
+	 *
+	 * @return string
+	 */
+	public static function get_content($args = []) {
+		ob_start();
+		include_once dtlms_get_template('course/tags');
+		return ob_get_clean();
+	}	
+
     public function render( $attr, $content = null, $render_slug) {
-        return " course tags ";
+
+		$output = self::get_content($this->props);
+
+		// Render empty string if no output is generated to avoid unwanted vertical space.
+		if ('' === $output) {
+			return '';
+		}
+
+		return $this->_render_module_wrapper($output, $render_slug);
     }
 
 }
