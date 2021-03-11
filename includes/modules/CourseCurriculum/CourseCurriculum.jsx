a7ef2537911aebe15bab5b503bb69d012500bcee
Shewa <shewa@Shewas-iMac.local>
2021-03-11 17:59:22 +0600

    course curriculum files added

---
+++ includes/modules/CourseCurriculum/CourseCurriculum.jsx
@@ -0,0 +1,18 @@
+
+import React, {Component, Fragment} from 'react';
+
+class CourseCurriculum extends Component {
+
+    static slug = 'tutor_course_curriculum';
+
+    render(){
+        return (
+            <Fragment>
+                <h1> {this.props.tutor_course_list_heading_new} </h1>
+                <p> {this.props.content()} </p>
+            </Fragment>
+        );
+    }
+}
+
+export default CourseCurriculum;
