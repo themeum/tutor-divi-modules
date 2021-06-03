06745ac92e9a1034625777c5126d691a4ea22fc6
Shewa <shewa@Shewas-iMac.local>
2021-06-03 11:42:40 +0600

    default border removed from course benefit

--- includes/modules/CourseBenefits/CourseBenefits.php
+++ includes/modules/CourseBenefits/CourseBenefits.php
@@ -78,7 +78,7 @@ class TutorCourseBenefits extends ET_Builder_Module {
 				),
 			),
 			'borders'    => array(
-				'default'            => array(),
+				'default'            => false,
 				'list'              => array(
 					'css'             	=> array(
 						'main' => array(
@@ -243,7 +243,7 @@ class TutorCourseBenefits extends ET_Builder_Module {
 					'step'	=> '1'
 				),
 				'tab_slug'			=> 'advanced',
-				'toggle_slug'		=> 'text',
+				'toggle_slug'		=> 'benefit_text',
 				'mobile_options'	=> true
 			),
 		);
