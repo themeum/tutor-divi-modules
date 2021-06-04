eecdab75de96bbbe4404bf3b3acfa515d2b17c01
Shewa <shewa@Shewas-iMac.local>
2021-06-04 11:42:38 +0600

    twice text settings from requirements removed

--- includes/modules/CourseRequirements/CourseRequirements.php
+++ includes/modules/CourseRequirements/CourseRequirements.php
@@ -48,7 +48,7 @@ class TutorCourseRequirements extends ET_Builder_Module {
 					'icon' => array(
 						'title'		=> esc_html__('Icon', 'tutor-divi-modules'),
 					),
-					'text' => array(
+					'requirements_text' => array(
 						'title'		=> esc_html__('Text', 'tutor-divi-modules'),
 					),
 				),
@@ -70,13 +70,13 @@ class TutorCourseRequirements extends ET_Builder_Module {
 					'tab_slug'     		=> 'advanced',
 					'toggle_slug'  		=> 'title',
 				),
-				'text' => array(
+				'requirements_text' => array(
 					'css'          		=> array(
 						'main' => $li_selector,
 					),
 					'hide_text_align'	=> true,
 					'tab_slug'     		=> 'advanced',
-					'toggle_slug'  		=> 'text',
+					'toggle_slug'  		=> 'requirements_text',
 				),
 			),
 			'borders'    => array(
@@ -304,7 +304,7 @@ class TutorCourseRequirements extends ET_Builder_Module {
 					'step'	=> '1'
 				),
 				'tab_slug'			=> 'advanced',
-				'toggle_slug'		=> 'text',
+				'toggle_slug'		=> 'requirements_text',
 				'mobile_options'	=> true
 			),			
 		);
