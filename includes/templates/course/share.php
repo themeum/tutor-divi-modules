a9d81797b29efa73cf31d23a60d5865ec3568eef
Shewa <shewa@Shewas-iMac.local>
2021-06-08 18:26:01 +0600

    share version added

--- includes/templates/course/share.php
+++ includes/templates/course/share.php
@@ -5,12 +5,19 @@ $label                  = $args['share_label'];
 if ( !$disable_course_share){ ?>
 <div class="tutor-single-course-meta tutor-meta-top">
     <ul>
-        <li class="tutor-social-share">
-           <?php if( $label == 'on'): ?>
-            <span><?php _e('Share:', 'tutor'); ?></span>
-           <?php endif; ?>
-            <?php tutor_social_share(); ?>
-        </li>
+        <?php if( version_compare( tutor()->version, '1.9.1' ) > '1.9.1' ) : ?>
+            <li class="tutor-social-share">
+                <?php tutor_social_share(); ?>
+            </li>
+        <?php else : ?>
+            <li class="tutor-social-share">
+                <?php if( $label == 'on'): ?>
+                <span><?php _e('Share:', 'tutor'); ?></span>
+                <?php endif; ?>
+                <?php tutor_social_share(); ?>
+            </li>
+        <?php endif;?>
     </ul>
 </div>
 <?php } ?>
+
