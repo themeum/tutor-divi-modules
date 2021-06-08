00aed5072e897ced8e0c81a2d60948e3da1e0194
Shewa <shewa@Shewas-iMac.local>
2021-06-08 18:06:48 +0600

    label on off check added

--- includes/templates/course/share.php
+++ includes/templates/course/share.php
@@ -1,12 +1,14 @@
 <?php
 global $post;
-$disable_course_share = get_tutor_option('disable_course_share');
-
+$disable_course_share   = get_tutor_option('disable_course_share');
+$label                  = $args['share_label'];
 if ( !$disable_course_share){ ?>
 <div class="tutor-single-course-meta tutor-meta-top">
     <ul>
         <li class="tutor-social-share">
+           <?php if( $label == 'on'): ?>
             <span><?php _e('Share:', 'tutor'); ?></span>
+           <?php endif; ?>
             <?php tutor_social_share(); ?>
         </li>
     </ul>
