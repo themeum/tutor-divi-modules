d752875209fe3d3c6cc560130923bff97264dee9
shewa12 <shewa12kpi@gmail.com>
2021-12-13 14:48:56 +0600

    code formated

--- includes/modules/CoursePrice/CoursePrice.php
+++ includes/modules/CoursePrice/CoursePrice.php
@@ -2,6 +2,7 @@
 
 /**
  * Tutor Course Price Module for Divi Builder
+ *
  * @since 1.0.0
  * @author Themeum<www.themeum.com>
  */
@@ -12,9 +13,9 @@ defined( 'ABSPATH' ) || exit;
 
 class CoursePrice extends ET_Builder_Module {
 
-    // Module slug (also used as shortcode tag)
-    public $slug        = 'tutor_course_price';
-    public $vb_support  = 'on';
+	// Module slug (also used as shortcode tag)
+	public $slug       = 'tutor_course_price';
+	public $vb_support = 'on';
 
 	// Module Credits (Appears at the bottom of the module settings modal)
 	protected $module_credits = array(
@@ -27,44 +28,44 @@ class CoursePrice extends ET_Builder_Module {
 	 *
 	 * @since 1.0.0
 	 */
-    public function init() {
-        $this->name         = esc_html__( 'Tutor Course Price', 'tutor-lms-divi-modules' ); 
-        $this->icon_path	= plugin_dir_path( __FILE__ ) . 'icon.svg';
+	public function init() {
+		$this->name      = esc_html__( 'Tutor Course Price', 'tutor-lms-divi-modules' );
+		$this->icon_path = plugin_dir_path( __FILE__ ) . 'icon.svg';
 
-		//settings toggles
+		// settings toggles
 		$this->settings_modal_toggles = array(
-			'general'  => array(
+			'general' => array(
 				'toggles' => array(
-					'main_content' => esc_html__('Content', 'tutor-lms-divi-modules'),
+					'main_content' => esc_html__( 'Content', 'tutor-lms-divi-modules' ),
 				),
-			)
+			),
 		);
 
-		$selector 		= '%%order_class%% .price >ins, %%order_class%% .price > span' ;
+		$selector       = '%%order_class%% .price >ins, %%order_class%% .price > span';
 		$hover_selector = '%%order_class%% .price:hover';
 
 		$this->advanced_fields = array(
-			'fonts'          => array(
+			'fonts'      => array(
 
-				'normal_style' 	=> array(
-					'css'          => array(
+				'normal_style' => array(
+					'css'             => array(
 						'main' => $selector,
 					),
-					'hide_text_align'	=> true,
-					'tab_slug'     		=> 'advanced',
-					
+					'hide_text_align' => true,
+					'tab_slug'        => 'advanced',
+
 				),
 
 			),
-			'button'			=> false,
-			'text'				=> false,
-			'borders'			=> false,
-			'max_width'			=> false,
-			'animation'			=> false,
-			'transform'			=> false,
-			//'background'		=> false,
-			'filters'			=> false,
-			'box_shadow'		=> false					 
+			'button'     => false,
+			'text'       => false,
+			'borders'    => false,
+			'max_width'  => false,
+			'animation'  => false,
+			'transform'  => false,
+			// 'background'      => false,
+			'filters'    => false,
+			'box_shadow' => false,
 		);
 	}
 
@@ -75,9 +76,9 @@ class CoursePrice extends ET_Builder_Module {
 	 *
 	 * @return array
 	 */
-    public function get_fields() {
+	public function get_fields() {
 		return array(
-			'course'       	=> Helper::get_field(
+			'course'    => Helper::get_field(
 				array(
 					'default'          => Helper::get_course_default(),
 					'computed_affects' => array(
@@ -85,152 +86,143 @@ class CoursePrice extends ET_Builder_Module {
 					),
 				)
 			),
-			'__price'		=> array(
-				'type'					=> 'computed',
-				'computed_callback'		=> array(
+			'__price'   => array(
+				'type'                => 'computed',
+				'computed_callback'   => array(
 					'CoursePrice',
-					'get_content'
+					'get_content',
 				),
-				'computed_depends_on'	=> array( 'course' ),
-				'computed_minimum'		=> array( 'course' ),
+				'computed_depends_on' => array( 'course' ),
+				'computed_minimum'    => array( 'course' ),
 			),
-			'alignment'		=> array(
-				'label'				=> esc_html__('Alignment', 'tutor-lms-divi-modules'),
-				'type'				=> 'text_align',
-				'option_category'	=> 'configuration',
-				'options'			=> et_builder_get_text_orientation_options( array( 'justified' ) ),
-				'default'			=> 'left',
-				'toggle_slug'		=> 'main_content',
-				'mobile_options'	=> true
+			'alignment' => array(
+				'label'           => esc_html__( 'Alignment', 'tutor-lms-divi-modules' ),
+				'type'            => 'text_align',
+				'option_category' => 'configuration',
+				'options'         => et_builder_get_text_orientation_options( array( 'justified' ) ),
+				'default'         => 'left',
+				'toggle_slug'     => 'main_content',
+				'mobile_options'  => true,
 			),
 
 		);
-    }
-
-
-	/**
-	 * computed value
-	 * @since 1.0.0
-	 * @return string | array course level
-	 */
-	public static function get_props( $args = [] ) {
-		$course_id 	= $args[ 'course' ];
-		$price		= tutor_utils()->get_raw_course_price( $course_id );
-		return $price ? $price : 'Free' ;
-	}	
+	}
 
 	/**
-	 * computed value
+	 * Computed value.
+	 *
+	 * @param array $args | settings control.
+	 *
 	 * @since 1.0.0
+	 *
 	 * @return string
 	 */
-	public static function get_content() {
+	public static function get_content( $args = array() ) {
 		ob_start();
-		tutor_course_price() ;
-		$price 		= ob_get_clean(); 	
-		$content = '<div class="tutor-divi-course-price"><div class="price"><span>'.$price.'</span></div></div>';
-		return $content;	
-	}	
+		$price   = tutor_course_price( $args['course'] );
+		$content = '<div class="tutor-divi-course-price"><div class="price"><span>' . $price . '</span></div></div>';
+		return ob_get_clean();
+	}
 
 	/**
-	 * Render module output
+	 * Render module output.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param array  $attrs       List of unprocessed attributes
-	 * @param string $content     Content being processed
-	 * @param string $render_slug Slug of module that is used for rendering output
+	 * @param array  $attr | List of unprocessed attributes.
+	 * @param string $content | Content being processed.
+	 * @param string $render_slug | Slug of module that is used for rendering output.
 	 *
 	 * @return string module's rendered output
 	 */
-    public function render( $attr, $content, $render_slug) {
-		//selectors
-		$selector 		= '%%order_class%% .price .price';
+	public function render( $attr, $content, $render_slug ) {
+		// selectors
+		$selector = '%%order_class%% .price .price';
 
-		//props
-		$alignment		= sanitize_text_field( $this->props['alignment'] );
-		$alignment_tablet	= isset( $this->props['alignment_tablet'] ) && '' !== $this->props['alignment_tablet'] ? sanitize_text_field( $this->props['alignment_tablet'] ) : $alignment;
-		$alignment_phone	= isset( $this->props['alignment_phone'] ) && '' !== $this->props['alignment_phone'] ? sanitize_text_field( $this->props['alignment_phone'] ) : $alignment;	
+		// props
+		$alignment        = sanitize_text_field( $this->props['alignment'] );
+		$alignment_tablet = isset( $this->props['alignment_tablet'] ) && '' !== $this->props['alignment_tablet'] ? sanitize_text_field( $this->props['alignment_tablet'] ) : $alignment;
+		$alignment_phone  = isset( $this->props['alignment_phone'] ) && '' !== $this->props['alignment_phone'] ? sanitize_text_field( $this->props['alignment_phone'] ) : $alignment;
 
-		if( 'left' === $alignment) {
+		if ( 'left' === $alignment ) {
 			$alignment = 'flex-end';
-		} else if ( 'right' === $alignment ) {
+		} elseif ( 'right' === $alignment ) {
 			$alignment = 'flex-start';
-		}		
+		}
 
-		if( 'left' === $alignment_tablet) {
+		if ( 'left' === $alignment_tablet ) {
 			$alignment_tablet = 'flex-end';
-		} else if ( 'right' === $alignment_tablet ) {
+		} elseif ( 'right' === $alignment_tablet ) {
 			$alignment_tablet = 'flex-start';
-		}		
+		}
 
-		if( 'left' === $alignment_phone) {
+		if ( 'left' === $alignment_phone ) {
 			$alignment_phone = 'flex-end';
-		} else if ( 'right' === $alignment_phone ) {
+		} elseif ( 'right' === $alignment_phone ) {
 			$alignment_phone = 'flex-start';
 		}
 
-		//set styles
+		// set styles
 
 		ET_Builder_Element::set_style(
 			$render_slug,
 			array(
-				'selector'		=> $selector,
-				'declaration'	=> 'display: flex; flex-direction: row-reverse;'
+				'selector'    => $selector,
+				'declaration' => 'display: flex; flex-direction: row-reverse;',
 			)
-		);		
+		);
 
-		if( '' !== $alignment ) {
+		if ( '' !== $alignment ) {
 			ET_Builder_Element::set_style(
 				$render_slug,
 				array(
-					'selector'		=> $selector,
-					'declaration'	=> sprintf(
+					'selector'    => $selector,
+					'declaration' => sprintf(
 						'justify-content: %1$s;',
 						$alignment
-					) 
+					),
 				)
 			);
 		}
-		if( '' !== $alignment_tablet ) {
+		if ( '' !== $alignment_tablet ) {
 			ET_Builder_Element::set_style(
 				$render_slug,
 				array(
-					'selector'		=> $selector,
-					'declaration'	=> sprintf(
+					'selector'    => $selector,
+					'declaration' => sprintf(
 						'justify-content: %1$s;',
 						$alignment_tablet
 					),
-					'media_query'	=> ET_Builder_Element::get_media_query('max_width_980')
+					'media_query' => ET_Builder_Element::get_media_query( 'max_width_980' ),
 				)
 			);
 		}
-		if( '' !== $alignment_phone ) {
+		if ( '' !== $alignment_phone ) {
 			ET_Builder_Element::set_style(
 				$render_slug,
 				array(
-					'selector'		=> $selector,
-					'declaration'	=> sprintf(
+					'selector'    => $selector,
+					'declaration' => sprintf(
 						'justify-content: %1$s;',
 						$alignment_phone
-					), 
-					'media_query'	=> ET_Builder_Element::get_media_query('max_width_767')
+					),
+					'media_query' => ET_Builder_Element::get_media_query( 'max_width_767' ),
 				)
 			);
 		}
-		
+
 		ET_Builder_Element::set_style(
 			$render_slug,
 			array(
-				'selector'		=> '%%order_class%% .price del span bdi span, %%order_class%% .price del span bdi',
-				'declaration'	=> 'font-size: 14px !important;'
-			)			
+				'selector'    => '%%order_class%% .price del span bdi span, %%order_class%% .price del span bdi',
+				'declaration' => 'font-size: 14px !important;',
+			)
 		);
 
-		//set styles end
-		$output 	= self::get_content();
-		return $this->_render_module_wrapper($output, $render_slug);
-    }
+		// set styles end
+		$output = self::get_content( $this->props );
+		return $this->_render_module_wrapper( $output, $render_slug );
+	}
 
 }
-new CoursePrice;
+new CoursePrice();
