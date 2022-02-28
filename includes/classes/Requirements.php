6a4cc900468b79599129571ed17fd7ac0dd879ed
shewa12 <shewa12kpi@gmail.com>
2022-02-28 16:48:27 +0600

    fix: space and indentation error

--- includes/classes/Requirements.php
+++ includes/classes/Requirements.php
@@ -2,9 +2,10 @@
 /**
  * Tutor Divi Modules package
  * check plugin requirements/dependency
+ *
  * @since 1.0.0
  * @author Themeum <www.themeum.com>
-*/
+ */
 
 namespace TutorLMS\Divi;
 
@@ -14,46 +15,50 @@ class Requirements {
 
 	/**
 	 * trigger require hooks on class init
+	 *
 	 * @since 1.0.0
-	*/
+	 */
 	public function __construct() {
 
-		add_action('admin_init', [$this, 'check_plugin_dependency']);	
-		add_action('admin_enqueue_scripts', [$this, 'admin_enqueue_scripts'], 99);	
+		add_action( 'admin_init', array( $this, 'check_plugin_dependency' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 99 );
 	}
 
 	/**
 	 * check dependency
+	 *
 	 * @since 1.0.0
-	*/
+	 */
 	public function check_plugin_dependency() {
-		if (!defined('TUTOR_VERSION')) {
-            //Required Tutor Message
-            add_action('admin_notices', array($this, 'required_tutor_lms'));
-        }
+		if ( ! defined( 'TUTOR_VERSION' ) ) {
+			// Required Tutor Message
+			add_action( 'admin_notices', array( $this, 'required_tutor_lms' ) );
+		}
 	}
 
 	/**
 	 * generate admin notice
+	 *
 	 * @since 1.0.0
-	*/
-    public function required_tutor_lms() {
-    	include_once dtlms_get_template('admin_notice');
+	 */
+	public function required_tutor_lms() {
+		include_once dtlms_get_template( 'admin_notice' );
 	}
 
-    /**
-     * Enqueue admin styles
-     * @since 1.0.0
-     */
-    public function admin_enqueue_scripts() {
-        wp_enqueue_style(
-            'tutor-divi-admin-notice-min-css',
-            DTLMS_ASSETS . 'css/admin_notice.min.css',
-            null,
-            DTLMS_VERSION
-        );
+	/**
+	 * Enqueue admin styles
+	 *
+	 * @since 1.0.0
+	 */
+	public function admin_enqueue_scripts() {
+		wp_enqueue_style(
+			'tutor-divi-admin-notice-min-css',
+			DTLMS_ASSETS . 'css/admin_notice.min.css',
+			null,
+			DTLMS_VERSION
+		);
 
-    }	
+	}
 }
 
-new Requirements;
+new Requirements();
