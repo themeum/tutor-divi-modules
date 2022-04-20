f7856d2af399e5c16c435adbfa4a7e17a76d87a6
Mohammad Ibrahim <ikardi04@gmail.com>
2022-04-20 10:43:58 +0600

    guest mode checkout issue fixed

--- includes/templates/course/add-to-cart-woocommerce.php
+++ includes/templates/course/add-to-cart-woocommerce.php
@@ -1,52 +1,75 @@
 <?php
 /**
+ * A single course loop add to cart
+ *
+ * @since v.1.0.0
+ * @author themeum
+ * @url https://themeum.com
+ *
  * @package TutorLMS/Templates
  * @version 1.4.3
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-$product_id = isset( $data['product_id'] ) ? $data['product_id']: tutor_utils()->get_course_product_id();
-$product    = function_exists( 'wc_get_product' ) ? wc_get_product( $product_id ) : '';
-if ( '' === $product ) {
-	esc_html_e( 'Woocommerce product not found.', 'tutor-lms-divi-modules' );
+$product_id = tutor_utils()->get_course_product_id();
+$product = wc_get_product($product_id);
+
+if (! $product_id || ! $product){
 	return;
 }
-if ( $product ) {
-	if ( tutor_utils()->is_course_added_to_cart( $product_id, true ) ) {
-		?>
-			<a href="<?php echo wc_get_cart_url(); ?>" class="tutor-btn tutor-btn-tertiary tutor-is-outline tutor-btn-lg tutor-btn-full">
-				<?php _e( 'View Cart', 'tutor' ); ?>
-			</a>
-		<?php
-	} else {
-		$sale_price    = $product->get_sale_price();
-		$regular_price = $product->get_regular_price();
-		$symbol        = get_woocommerce_currency_symbol();
-		?>
-		<div class="tutor-course-sidebar-card-pricing tutor-bs-d-flex align-items-end tutor-bs-justify-content-between">
-			<div>
-				<span class="tutor-text-bold-h4 tutor-color-text-primary">
-					<?php echo $symbol . ( $sale_price ? $sale_price : $regular_price ); ?>
-				</span>
-				<?php if ( $regular_price && $sale_price && $sale_price != $regular_price ) : ?>
-					<del class="tutor-text-regular-caption tutor-color-text-hints tutor-ml-7">
-						<?php echo $symbol . $regular_price; ?>
-					</del>
-				<?php endif; ?>
-			</div>
-		</div>
-		<form action="<?php echo esc_url( apply_filters( 'tutor_course_add_to_cart_form_action', get_permalink( get_the_ID() ) ) ); ?>" method="post" enctype="multipart/form-data">
-			<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>"  class="tutor-btn tutor-btn-icon tutor-btn-primary tutor-btn-lg tutor-btn-full tutor-mt-24 tutor-add-to-cart-button">
-				<span class="btn-icon tutor-icon-cart-filled"></span>
-				<span><?php echo esc_html( $product->single_add_to_cart_text() ); ?></span>
-			</button>
-		</form>
-		<?php
-	}
+
+/**
+ * Add required loggedin class
+ * @since v 1.5.5
+ */
+$isLoggedIn = is_user_logged_in();
+$enable_guest_course_cart = tutor_utils()->get_option('enable_guest_course_cart');
+$required_loggedin_class = '';
+$ajax_add_to_cart_class = '';
+if ( ! $isLoggedIn && ! $enable_guest_course_cart){
+	$required_loggedin_class = apply_filters('tutor_enroll_required_login_class', 'cart-required-login');
 } else {
-	?>
-	<p class="tutor-alert-warning">
-		<?php _e( 'Please make sure that your product exists and valid for this course', 'tutor' ); ?>
-	</p>
-	<?php
+	$ajax_add_to_cart_class = $product->supports( 'ajax_add_to_cart' ) ? 'ajax_add_to_cart' : '';
 }
+
+$args = array();
+$defaults = array(
+	'quantity'   => 1,
+	'class'      => implode( ' ', array_filter( array(
+		'button',
+		'product_type_' . $product->get_type(),
+		$product->is_purchasable() && $product->is_in_stock() ? 'tutor-btn tutor-btn-icon tutor-btn-disable-outline tutor-btn-ghost tutor-no-hover tutor-btn-md add_to_cart_button' : '',
+		$ajax_add_to_cart_class,
+		$required_loggedin_class
+	) ) ),
+	'attributes' => array(
+		'data-product_id'  => $product->get_id(),
+		'data-product_sku' => $product->get_sku(),
+		'aria-label'       => $product->add_to_cart_description(),
+		'rel'              => 'nofollow',
+	),
+);
+
+$args = apply_filters( 'woocommerce_loop_add_to_cart_args', wp_parse_args( $args, $defaults ), $product );
+
+if ( isset( $args['attributes']['aria-label'] ) ) {
+	$args['attributes']['aria-label'] = strip_tags( $args['attributes']['aria-label'] );
+}
+
+?>
+
+	<?php
+		echo apply_filters( 'tutor_course_restrict_new_entry', apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+			sprintf( '<a href="%s" data-quantity="%s" class="%s" %s><span class="btn-icon tutor-icon-cart-line-filled"></span><span class="cart-text">%s</span></a>',
+				esc_url( $product->add_to_cart_url() ),
+				esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+				esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+				isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+				esc_html( $product->add_to_cart_text() )
+			),
+			$product, $args ) );
+	?>
+
