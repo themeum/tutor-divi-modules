9050e5ee3783c8f77255359583770612b6be770f
shewa12 <shewa12kpi@gmail.com>
2022-06-20 14:28:07 +0600

    fix: coding errors

--- includes/modules/CourseThumbnail/CourseThumbnail.php
+++ includes/modules/CourseThumbnail/CourseThumbnail.php
@@ -2,6 +2,7 @@
 
 /**
  * Tutor Course Thumbnail Module for Divi Builder
+ *
  * @since 1.0.0
  * @author Themeum<www.themeum.com>
  */
@@ -15,62 +16,62 @@ class CourseThumbnail extends ET_Builder_Module {
 	public $slug       = 'tutor_course_thumbnail';
 	public $vb_support = 'on';
 
-    /**
-     * Module properties initialization
-     *
-     * @since 1.0.0
-     */
+	/**
+	 * Module properties initialization
+	 *
+	 * @since 1.0.0
+	 */
 	public function init() {
-		$this->name = esc_html__( 'Tutor Course Thumbnail', 'tutor-lms-divi-modules' );
-        $this->icon_path    = plugin_dir_path( __FILE__ ) . 'icon.svg';
-        $wrapper 				= '%%order_class%% .tutor-divi-course-thumbnail';
-        $this->advanced_fields = array(
-            'fonts'     => false,
-            'text'      => array(
-                'css'   => array(
-                    'main'  => $wrapper
-                )
-            ),
-            'borders'   => array(
-                'default'   => array(
-                    'css'       => array(
-                        'main'  => array(
-                            'border_radii'     => $wrapper,
-                            'border_styles'    => $wrapper
-                        )
-                    )
-                )
-            ),
-            'box_shadow'    => array(
-                'default'   => array(
-                    'css'   => array(
-                        'main'  => $wrapper
-                    )
-                )
-            ),
-            'max_width' => array(
-                'css'   => array(
-                    'module_alignment'  => $wrapper
-                )
-            ),
-            'text'          => false,
-            'button'        => false,
-            'background'    => false,
-            'filters'       => false,
-            'animation'     => false,
-        );
+		$this->name            = esc_html__( 'Tutor Course Thumbnail', 'tutor-lms-divi-modules' );
+		$this->icon_path       = plugin_dir_path( __FILE__ ) . 'icon.svg';
+		$wrapper               = '%%order_class%% .tutor-divi-course-thumbnail';
+		$this->advanced_fields = array(
+			'fonts'      => false,
+			'text'       => array(
+				'css' => array(
+					'main' => $wrapper,
+				),
+			),
+			'borders'    => array(
+				'default' => array(
+					'css' => array(
+						'main' => array(
+							'border_radii'  => $wrapper,
+							'border_styles' => $wrapper,
+						),
+					),
+				),
+			),
+			'box_shadow' => array(
+				'default' => array(
+					'css' => array(
+						'main' => $wrapper,
+					),
+				),
+			),
+			'max_width'  => array(
+				'css' => array(
+					'module_alignment' => $wrapper,
+				),
+			),
+			'text'       => false,
+			'button'     => false,
+			'background' => false,
+			'filters'    => false,
+			'animation'  => false,
+		);
 	}
 
-    /**
-     * Module's specific fields
-     *
-     * @since 1.0.0
-     *
-     * @return array
-     */
+	/**
+	 * Module's specific fields
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array
+	 */
 	public function get_fields() {
 		return array(
-			'course'       	=> Helper::get_field(
+			'course'      => Helper::get_field(
 				array(
 					'default'          => Helper::get_course_default(),
 					'computed_affects' => array(
@@ -78,14 +79,14 @@ class CourseThumbnail extends ET_Builder_Module {
 					),
 				)
 			),
-			'__thumbnail'		=> array(
+			'__thumbnail' => array(
 				'type'                => 'computed',
 				'computed_callback'   => array(
 					'CourseThumbnail',
 					'get_props',
 				),
 				'computed_depends_on' => array(
-					'course'
+					'course',
 				),
 				'computed_minimum'    => array(
 					'course',
@@ -96,71 +97,73 @@ class CourseThumbnail extends ET_Builder_Module {
 	}
 
 	/**
-     * get require props
+	 * get require props
+	 *
 	 * @since 1.0.0
 	 * @return string | array
 	 */
-	public static function get_props( $args = [] ) {
-		$course_id 			= $args[ 'course' ];
-        $thumbnail          = '' ;
-        $has_video          = get_post_meta( $course_id, '_video', true );
-        $source             = '';
-        if($has_video['source'] == '-1'){
-            $thumbnail = get_the_post_thumbnail_url( $course_id, $size = 'post-thumbnail');
+	public static function get_props( $args = array() ) {
+		$course_id = $args['course'];
+		$thumbnail = '';
+		$has_video = get_post_meta( $course_id, '_video', true );
+		$source    = '';
+		if ( $has_video['source'] == '-1' ) {
+			$thumbnail = get_the_post_thumbnail_url( $course_id, $size = 'post-thumbnail' );
 
-        } else{
-            $video = $has_video;
-            if($video && '' !== $video['source_youtube']) {
-                $thumbnail  = $video['source_youtube'];
-                $source     = 'youtube';
-            } else if( $video && '' !== $video['source_vimeo']) {
-                $thumbnail = $video['source_vimeo'];
-                $source     = 'vimeo';
-            }           
-        }
-        $props  = array(
-            'url'  => $thumbnail,
-            'has_video' => $has_video['source'] == '-1' ? false : true,
-            'source'    => $source
-        );
-        return $props;
+		} else {
+			$video = $has_video;
+			if ( $video && '' !== $video['source_youtube'] ) {
+				$thumbnail = $video['source_youtube'];
+				$source    = 'youtube';
+			} elseif ( $video && '' !== $video['source_vimeo'] ) {
+				$thumbnail = $video['source_vimeo'];
+				$source    = 'vimeo';
+			}
+		}
+		$props = array(
+			'url'       => $thumbnail,
+			'has_video' => $has_video['source'] == '-1' ? false : true,
+			'source'    => $source,
+		);
+		return $props;
 	}
 
-    /**
-     * Get the tutor course content
-     * @since 1.0.0
-     * @return string
-     */
-    public static function get_content( $args = []) {
-        ob_start();
-        if ($args['course']) {
-    
-            echo "<div class='tutor-divi-course-thumbnail'>";
-            if(tutils()->has_video_in_single()){
-                tutor_course_video();
-            } else{
-                get_tutor_course_thumbnail();
-            }
-            echo '</div>';
-        }
-       return ob_get_clean();
-    }
+	/**
+	 * Get the tutor course content
+	 *
+	 * @since 1.0.0
+	 * @return string
+	 */
+	public static function get_content( $args = array() ) {
+		ob_start();
+		if ( $args['course'] ) {
 
-    /**
-     * Render module output
-     *
-     * @since 1.0.0
-     *
-     * @param array  $attrs       List of unprocessed attributes
-     * @param string $content     Content being processed
-     * @param string $render_slug Slug of module that is used for rendering output
-     *
-     * @return string module's rendered output
-     */
+			echo "<div class='tutor-divi-course-thumbnail'>";
+			if ( tutils()->has_video_in_single() ) {
+				tutor_course_video();
+			} else {
+				get_tutor_course_thumbnail();
+			}
+			echo '</div>';
+		}
+		return ob_get_clean();
+	}
+
+	/**
+	 * Render module output
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array  $attrs       List of unprocessed attributes
+	 * @param string $content     Content being processed
+	 * @param string $render_slug Slug of module that is used for rendering output
+	 *
+	 * @return string module's rendered output
+	 */
 	public function render( $unprocessed_props, $content, $render_slug ) {
-        $output = self::get_content( $this->props );
-        return $this->_render_module_wrapper( $output, $render_slug );
+		$output = self::get_content( $this->props );
+		return $this->_render_module_wrapper( $output, $render_slug );
 	}
 }
 
-new CourseThumbnail;
+new CourseThumbnail();
