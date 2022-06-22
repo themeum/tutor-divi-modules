f4651144db192844f4ac55f890507050425ed4ff
shewa12 <shewa12kpi@gmail.com>
2022-06-22 12:09:08 +0600

    update: course list grid layout design updated

--- includes/templates/course/list/grid/overlayed.php
+++ includes/templates/course/list/grid/overlayed.php
@@ -1,19 +1,56 @@
-<?php $animation_class  = 'elementor-animation-' . $settings['course_list_card_hover_animation'] . $settings['card_hover_animation'] ? ' dtlms-has-hover-animation' : ''; ?>
+<?php $animation_class  = ''; ?>
 <div class="tutor-course-card dtlms-course-card-overlay <?php echo $animation_class; ?>">
     <?php
-        include dtlms_get_template( 'course/list/grid/parts/thumbnail' );
+        $header_templates = array(
+            DTLMS_TEMPLATES . 'list/grid/parts/thumbnail.php',
+        );
+        foreach ( $header_templates as $template ) {
+            if ( file_exists( $template ) ) {
+                tutor_load_template_from_custom_path(
+                    $template,
+                    $data,
+                    false
+                );
+            } else {
+                echo esc_html( $template ) . __( 'not found', 'tutor-lms-divi-modules' );
+            }
+        }
     ?>
 
     <div class="tutor-card tutor-loop-course-container">
         <div class="tutor-card-body">
             <?php
-                include dtlms_get_template( 'course/list/parts/ratings' );
-                include dtlms_get_template( 'course/list/parts/title' );
-                include dtlms_get_template( 'course/list/parts/meta' );
-                include dtlms_get_template( 'course/list/parts/info' );
+                $body_templates = array(
+                    DTLMS_TEMPLATES . 'list/parts/ratings.php',
+                    DTLMS_TEMPLATES . 'list/parts/title.php',
+                    DTLMS_TEMPLATES . 'list/parts/meta.php',
+                    DTLMS_TEMPLATES . 'list/parts/info.php',
+                );
+                foreach ( $body_templates as $template ) {
+                    if ( file_exists( $template ) ) {
+                        tutor_load_template_from_custom_path(
+                            $template,
+                            $data,
+                            false
+                        );
+                    } else {
+                        echo esc_html( $template ) . __( 'not found', 'tutor-lms-divi-modules' );
+                    }
+                }
             ?>
         </div>
 
-        <?php include dtlms_get_template( 'course/list/parts/footer' ); ?>
+        <?php
+            $footer_template = DTLMS_TEMPLATES . 'list/parts/footer.php';
+            if ( file_exists( $footer_template ) ) {
+                tutor_load_template_from_custom_path(
+                    $footer_template,
+                    $data,
+                    false
+                );
+            } else {
+                echo esc_html( $footer_template ) . __( 'not found', 'tutor-lms-divi-modules' );
+            }
+        ?>
     </div>
 </div>
--- includes/templates/course/list/grid/stacked.php
+++ includes/templates/course/list/grid/stacked.php
@@ -1,11 +1,24 @@
-<?php $animation_class  = 'elementor-animation-' . $settings['course_list_card_hover_animation'] . $settings['card_hover_animation'] ? ' dtlms-has-hover-animation' : ''; ?>
-<div class="tutor-course-card dtlms-course-grid-stacked<?php echo $animation_class; ?>">
+<?php $animation_class  = 'dtlms-has-hover-animation'; ?>
+<div class="tutor-course-card dtlms-course-grid-stacked <?php echo $animation_class; ?>">
     <div class="tutor-row tutor-gx-0">
         <div class="tutor-col-lg-4">
             <?php
-                include dtlms_get_template( 'course/list/grid/parts/thumbnail' );
-                include dtlms_get_template( 'course/list/parts/wishlist' );
-                include dtlms_get_template( 'course/list/parts/level' );
+                $header_templates = array(
+                    DTLMS_TEMPLATES . 'list/grid/parts/thumbnail.php',
+                    DTLMS_TEMPLATES . 'list/parts/wishlist.php',
+                    DTLMS_TEMPLATES . 'list/parts/level.php',
+                );
+                foreach ( $header_templates as $template ) {
+                    if ( file_exists( $template ) ) {
+                        tutor_load_template_from_custom_path(
+                            $template,
+                            $data,
+                            false
+                        );
+                    } else {
+                        echo esc_html( $template ) . __( 'not found', 'tutor-lms-divi-modules' );
+                    }
+                }
             ?>
         </div>
 
@@ -13,14 +26,43 @@
             <div class="tutor-card dtlms-course-card-inner">
                 <div class="tutor-card-body">
                     <?php
-                        include dtlms_get_template( 'course/list/parts/ratings' );
-                        include dtlms_get_template( 'course/list/parts/title' );
-                        include dtlms_get_template( 'course/list/parts/meta' );
-                        include dtlms_get_template( 'course/list/parts/info' );
+                        // include dtlms_get_template( 'course/list/parts/ratings' );
+                        // include dtlms_get_template( 'course/list/parts/title' );
+                        // include dtlms_get_template( 'course/list/parts/meta' );
+                        // include dtlms_get_template( 'course/list/parts/info' );
+
+                        $body_templates = array(
+                            DTLMS_TEMPLATES . 'list/parts/ratings.php',
+                            DTLMS_TEMPLATES . 'list/parts/title.php',
+                            DTLMS_TEMPLATES . 'list/parts/meta.php',
+                            DTLMS_TEMPLATES . 'list/parts/info.php',
+                        );
+                        foreach ( $body_templates as $template ) {
+                            if ( file_exists( $template ) ) {
+                                tutor_load_template_from_custom_path(
+                                    $template,
+                                    $data,
+                                    false
+                                );
+                            } else {
+                                echo esc_html( $template ) . __( 'not found', 'tutor-lms-divi-modules' );
+                            }
+                        }
                     ?>
                 </div>
 
-                <?php include dtlms_get_template( 'course/list/parts/footer' ); ?>
+                <?php
+                    $footer_template = DTLMS_TEMPLATES . 'list/parts/footer.php';
+                    if ( file_exists( $footer_template ) ) {
+                        tutor_load_template_from_custom_path(
+                            $footer_template,
+                            $data,
+                            false
+                        );
+                    } else {
+                        echo esc_html( $footer_template ) . __( 'not found', 'tutor-lms-divi-modules' );
+                    }
+                ?>
             </div>
         </div>
     </div>
